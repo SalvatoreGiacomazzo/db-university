edba6ea65301e90d41429778c2bdb7e41b54b86e
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-10-18 18:13:57 +0200

    exercise finished no bonus

--- query-2.sql
+++ query-2.sql
@@ -12,13 +12,25 @@ WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di
 Neuroscienze
 */
-
+SELECT `degrees`.`name`, `degrees`.`level`, `departments`.`name`
+FROM `degrees`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+WHERE `degrees`.`level` = "Magistrale"
+AND `departments`.`name` = "Dipartimento di Neuroscienze"
 
 
 
 /*
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 */
+SELECT `teachers`.`id` AS `id_teacher`,`teachers`.`name`,`teachers`.`surname`,`courses`.*
+FROM `teachers`
+JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses` ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `teachers`.`id` = "44";
+
+
+
 
 
 /*
@@ -26,14 +38,37 @@ Neuroscienze
 sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e
 nome */
 
+SELECT `students`.`id`,  `students`.`name`, `students`.`surname`, `degrees`.`name`, `departments`.`name`
+FROM `students`
+JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`, `students`.`name` ASC;
+
+
+
 /*
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti*/
+SELECT `departments`.`name`, `degrees`.`name`, `courses`.`name`, `teachers`.`name`, `teachers`.`surname`
+FROM `degrees`
+JOIN `courses` ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers` ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `departments` ON `departments`.`id` = `degrees`.`department_id`
+
+
+
 
 
 /*
 6. Selezionare tutti i docenti che insegnano nel Dipartimento di
 Matematica (54)*/
-
+SELECT DISTINCT `teachers`.`name`, `teachers`.`surname`
+FROM `departments`
+JOIN `degrees` ON `departments`.`id` = `degrees`.`department_id`
+JOIN `courses` ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers` ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `departments`.`name` = "Dipartimento di Matematica";
 
 
 
@@ -81,6 +116,6 @@ ORDER BY (`average_vote`) DESC;
 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 */
 
-SELECT `degree_id`, COUNT(*) as `number_courses`
-FROM `courses` 
-GROUP BY `degree_id`;  
+SELECT `department_id`, COUNT(*) as `number_courses`
+FROM `degrees` 
+GROUP BY `department_id`;  
